bccfa6517958d1b9047ec7d2a35832b69e26d38e
Cory Wall <cwall4@mail.csuchico.edu>
2017-04-25 12:23:36 -0700

    fixed spacing

--- gallery.js
+++ gallery.js
@@ -1,5 +1,6 @@
 var chico = ["Upper Park", "Bidwell Mansion", "One Mile", "Sierra Nevada Brewery", "20th Street Park"];
 var current;
+
 function shuffleGallery() {
   do {
     var randIndex = (Math.floor(Math.random() * 100) % chico.length);
