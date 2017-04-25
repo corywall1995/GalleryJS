16a1e9e9a2f031aa25902765deeda627527e5cca
Cory Wall <cwall4@mail.csuchico.edu>
2017-04-25 12:21:30 -0700

    Created an array call chico populated array with string Created variable called current, contains the current element of the array that is being displayed Generates a random number that is of a valid index for the array. display the element of the array at index of random number to the console

--- gallery.js
+++ gallery.js
@@ -1,6 +1,9 @@
-
-
-function shuffleGallery()
-{
-
+var chico = ["Upper Park", "Bidwell Mansion", "One Mile", "Sierra Nevada Brewery", "20th Street Park"];
+var current;
+function shuffleGallery() {
+  do {
+    var randIndex = (Math.floor(Math.random() * 100) % chico.length);
+  } while (chico[randIndex] == current)
+  current = chico[randIndex];
+  console.log(chico[randIndex]);
 }
